a020bd6052f93ab1b6169fa9d8a8f43151b2bc36
Stefan Schauer <s-schauer1@ti.com>
2016-12-13 09:18:18 +0100

    use dslite for all devices updated detection of Cal values in value line devices

--- cores/msp430/wiring.c
+++ cores/msp430/wiring.c
@@ -259,16 +259,16 @@ void initClocks(void)
 {
 
 #ifdef __MSP430_HAS_BC2__
-#if defined(CALBC1_16MHZ_) && F_CPU >= 16000000L
+#if defined(CAL_BC1_16MHZ) && F_CPU >= 16000000L
 	BCSCTL1 = CALBC1_16MHZ;
 	DCOCTL = CALDCO_16MHZ;
-#elif defined(CALBC1_12MHZ_) && (F_CPU >= 12000000L)
+#elif defined(CAL_BC1_12MHZ) && (F_CPU >= 12000000L)
 	BCSCTL1 = CALBC1_12MHZ;
 	DCOCTL = CALDCO_12MHZ;
-#elif defined(CALBC1_8MHZ_) && (F_CPU >= 8000000L)
+#elif defined(CAL_BC1_8MHZ) && (F_CPU >= 8000000L)
 	BCSCTL1 = CALBC1_8MHZ;
 	DCOCTL = CALDCO_8MHZ;
-#elif defined(CALBC1_1MHZ_) && (F_CPU >= 1000000L)
+#elif defined(CAL_BC1_1MHZ) && (F_CPU >= 1000000L)
 	BCSCTL1 = CALBC1_1MHZ;
 	DCOCTL = CALDCO_1MHZ;
 #else
