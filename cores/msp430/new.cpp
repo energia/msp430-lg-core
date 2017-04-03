9b6fc95750b87eed164907902bfa23be29c01576
Rick Kimball <rick@kimballsoftware.com>
2017-04-03 13:36:22 -0400

    fix for Issue 15

--- cores/msp430/new.cpp
+++ cores/msp430/new.cpp
@@ -5,7 +5,17 @@ void * operator new(size_t size)
   return malloc(size);
 }
 
+void * operator new[](size_t size)
+{
+  return malloc(size);
+}
+
 void operator delete(void * ptr)
+{
+  free(ptr);
+} 
+
+void operator delete[](void * ptr)
 {
   free(ptr);
 } 
