edadadb22500c306acfe5445b7d331484fdddaee
StefanSch <s-schauer1@ti.com>
2021-04-06 09:57:46 +0200

    updated GCC versions

--- extras/versions.sh
+++ extras/versions.sh
@@ -9,22 +9,22 @@ ENERGIA_URL="http://energia.nu/downloads/download_core.php?file="
 # gcc compiler versions can be found here:
 # web page: http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/index_FDS.html
 # web page: https://www.ti.com/tool/download/MSP430-GCC-OPENSOURCE
-GCC_VER="9.2.0.50"
+GCC_VER="9.3.1.11"
 #MSPGCC_VER="6_01_01_000"
-MSPGCC_VER="9_2_0_0"
-MSPSUPPORT_VER="1.209"
+MSPGCC_VER="9_3_1_1"
+MSPSUPPORT_VER="1.211"
 
 # url where dslite should be grabed for the package:
 TOOLS_URL="https://s3.amazonaws.com/energiaUS/tools"
 DSLITE_VER="9.3.0.1863"
 
 # other tool versions:
-INO2CPP_VER="1.0.4"
+INO2CPP_VER="1.0.6"
 MSPDEBUG_VER="0.24"
 
 # New GCC Compiler (Mito)
 # Energia tools version number - stored in platform.txt
-ENERGIA_VER="2.0.10"
+ENERGIA_VER="2.0.12"
 
 
 # Legacy GCC Compiler
